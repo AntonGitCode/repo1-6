4d12339a7adba10aaa17c2df1607a7db73fd339c
AntonGitCode <117078390+AntonGitCode@users.noreply.github.com>
2022-12-09 19:03:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+  let swiper;
+
+  function swiperInit () {
+    swiper = new Swiper('.swiper', {
+      loop: false,
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+  
+  if (window.innerWidth<=768 && (typeof swiper === 'undefined')) {
+    swiperInit();
+  } 
+
+  tabletMedia.addEventListener('change', event => {
+    if (event.matches) {
+
+      if (!modal_close_btn.classList.contains('inside')) {
+        modal_close_btn.classList.add('inside');
+        if (!modal.classList.contains('block')) {
+          modal.classList.add('block');
+        }
+      }
+      swiperInit();
+    }      
+    else {
+      swiper[0].params.spaceBetween = 0;
+      swiper[1].params.spaceBetween = 0;
+      swiper.destroy;
+    }
+  });
+
+padMedia.addEventListener('change', event => 
+{ if (event.matches) { 
+    if (modal_close_btn.classList.contains('inside')) {
+      modal_close_btn.classList.remove('inside');
+    }
+  } 
+});
+
+deskMedia.addEventListener('change', event => { 
+  if (event.matches) {  
+    if (aside_menu.classList.contains('aside-fade-out')) {
+      aside_menu.classList.remove('aside-fade-out');
+    }
+    if (aside_menu.classList.contains('aside-fade-in')) {
+      aside_menu.classList.remove('aside-fade-in');
+    }
+    if (!screen_wrapper.classList.contains('shut') && (!modal.classList.contains('modal-aside-in'))) {
+      screen_wrapper.classList.add('shut');
+    }
+    if (!screen_wrapper.classList.contains('z-index')) {
+       screen_wrapper.classList.remove('z-index');
+    }
+  } 
+});
